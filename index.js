4a787a795e5d5b38fd116b68fa535e249a372384
AJ <ashishshetty2612@gmail.com>
2021-05-25 14:12:43 +0530

    initial setup

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
-
-console.log(process.env.PORT);
-
 const PORT = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+
+const { initializeDBConnection } = require("./database/db.connect");
+const { routeNotFound } = require("./middlewares/route-not-fount-middelware");
+const { errorHandler } = require("./middlewares/error-handler-middelware");
+
+const videoRouter = require("./routes/videos.router");
+const { populateVideoColletion } = require("./models/video.model");
+
+/**
+ * Database connection do not move the code.
+ */
+(async () => {
+  await initializeDBConnection();
+})();
+
+/**
+ * one time dump of video data
+ */
+
+//populateVideoColletion();
+
+app.get("/", (req, res) => {
+  res.json({ mssage: "welcome to fittube api explorer" });
+});
+
+app.use("/video", videoRouter);
+
+/**
+ * Route not found middleware do not move
+ */
+app.use(routeNotFound);
+
+/**
+ * Error handler middleware do not move
+ */
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
